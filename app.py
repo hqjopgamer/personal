91007fb8f2a541f2e4a7f5b1ff33047d2e96764e
hussain <106746088+hqjopgamer@users.noreply.github.com>
2024-12-29 10:37:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 # Helper function to get tasks
 def get_tasks():
